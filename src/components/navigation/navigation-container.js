1c125f3bb00ac2ee2fd57bee1798315e1981e9a3
Gaizka <gaizkamg@gmail.com>
2023-09-19 18:21:25 +0200

    Authetication added

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,28 +1,64 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { withRouter } from "react-router";
 import { NavLink } from "react-router-dom";
 
-class NavigationContainer extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
+const NavigationContainer = (props) => {
+  const dynamicLink = (route, linkText) => {
     return (
-      <div className="nav-wrapper">
-        <div className="left-side">
-          <div className="nav-link-wrapper">
-            <NavLink exact to="/" activeClassName="nav-link-active">
-              Home
-            </NavLink>
-            <NavLink to="/about-me" activeClassName="nav-link-active">About</NavLink>
-            <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
-            <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
-          </div>
-        </div>
-        <div class="right-side">GAIZKA MARTÍNEZ</div>
-      </div>
+      <NavLink to={route} activeClassName="nav-link-active">
+       {linkText}
+      </NavLink>
     );
-  }
-}
+  };
 
-export default NavigationContainer;
+  const handleSignOut = () => {
+    axios
+      .delete("https://api.devcamp.space/logout", { withCredentials: true })
+      .then((response) => {
+        if (response.status === 200) {
+          props.history.push("/");
+          props.handleSuccessfulLogout();
+        }
+        return response.data;
+      })
+      .catch((error) => {
+        console.error("ERROR signing out", error);
+      });
+  };
+  
+  return (
+    <div className="nav-wrapper">
+      <div className="left-side">
+        <div className="nav-link-wrapper">
+          <NavLink exact to="/" activeClassName="nav-link-active">
+            Home
+          </NavLink>
+          <NavLink to="/about-me" activeClassName="nav-link-active">
+            About
+          </NavLink>
+          <NavLink to="/contact" activeClassName="nav-link-active">
+            Contact
+          </NavLink>
+          <NavLink to="/blog" activeClassName="nav-link-active">
+            Blog
+          </NavLink>
+          {props.loggedInStatus === "LOGGED_IN"
+            ? dynamicLink("/portfolio-manager", "Portfolio Manager")
+            : null}
+          <NavLink to="/auth" activeClassName="nav-link-active">
+            Auth
+          </NavLink>
+        </div>
+      </div>
+      <div className="right-side">
+        GAIZKA MARTÍNEZ
+        {props.loggedInStatus === "LOGGED_IN" ? (
+          <a onClick={handleSignOut}>Sign Out</a>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(NavigationContainer);
---
+++ src/components/pages/portfolio-manager.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PortfolioManager extends Component {
+    render() {
+        return (
+            <div>
+                <h1>PortfolioManager</h1>
+                <h1>PortfolioManager</h1>
+            </div>
+        );
+    }
+}
+
+export default PortfolioManager;
