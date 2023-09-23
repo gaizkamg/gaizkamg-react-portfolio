4c3d47b53c65e9cc5c9960860efb6ca88f312ce4
Gaizka <gaizkamg@gmail.com>
2023-09-23 14:32:27 +0200

    Form structure

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 
 import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
+import PortfolioForm from "../portfolio/portfolio-form";
 
 class PortfolioManager extends Component {
   constructor() {
@@ -10,14 +11,28 @@ class PortfolioManager extends Component {
     this.state = {
       portfolioItems: [],
     };
+
+    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+    this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+
+  }
+
+  handleSuccessfulFormSubmission(portfolioItem) {
+    //TODO
+    // update the portfolioItems state
+    // add the portfolioItem to the list
+  }
+
+  handleFormSubmissionError(error) {
+    console.log("handleFormSubmissionError ERROR>>>", error);
   }
 
   getPortfolioItems() {
     axios
       .get("https://jordan.devcamp.space/portfolio/portfolio_items")
       .then((response) => {
-        this.setState({ 
-          portfolioItems: [...response.data.portfolio_items]
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items],
         });
       })
       .catch((error) => {
@@ -33,10 +48,16 @@ class PortfolioManager extends Component {
     return (
       <div className="portfolio-manager-wrapper">
         <div className="left-column">
-          <h1>Portfolio Form</h1>
+          <h1>
+            <PortfolioForm 
+              handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+              handleFormSubmissionError={this.handleFormSubmissionError}
+            
+            />
+          </h1>
         </div>
         <div className="right-column">
-          <PortfolioSidebarList data={this.state.portfolioItems}/>
+          <PortfolioSidebarList data={this.state.portfolioItems} />
         </div>
       </div>
     );
