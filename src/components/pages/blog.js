e483a53cd887d765eaa8f9cc3d5ba85fa6334463
Gaizka <gaizkamg@gmail.com>
2023-10-04 19:18:22 +0200

    state changes for the infinite scroll control

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -9,6 +9,9 @@ class Blog extends Component {
 
     this.state = {
       blogItems: [],
+      totalCount: 0,
+      currentPage: 0
+
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
@@ -27,6 +30,11 @@ class Blog extends Component {
   }
 
   getBlogItems() {
+    this.setState({
+      currentPage: this.state.currentPage +1
+    });
+
+
     axios
       .get("https://gaizkamg.devcamp.space/portfolio/portfolio_blogs", {
         withCredentials: true,
@@ -34,6 +42,7 @@ class Blog extends Component {
       .then((response) => {
         this.setState({
           blogItems: response.data.portfolio_blogs,
+          totalCount: response.data.meta.total_records
         });
       })
       .catch((error) => {
